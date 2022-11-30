316e13c97a17e2a4fd583ecb3a9478779b7b9413
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-30 16:24:44 +0900

    slick OK

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { productAction } from "../redux/actions/productAction";
 
+import Slider from "react-slick";
+
 const ProductDetail = () => {
   /* const dispatch = useDispatch();
   const product = useSelector((state) => state.product); */
@@ -25,35 +27,49 @@ const ProductDetail = () => {
     setProductList(data);
   }
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+
   useEffect(() => {
-    /* dispatch(productAction.getProduct());
-    console.log(product); */
     getProduct();
   },[id]);
 
   return (
-    <div className="wrap">
-      <h1>상품 디테일 페이지</h1>
-      <div>
-        {productList ? <p>{productList.id}</p> : ""}
-        {productList ? <p>{productList.productName}</p> : ""}
-        {productList ? <p>{productList.price}</p> : ""}
+    <div className="product-area">
+        <div className="product-detail">
+        {/* <h1>상품 디테일 페이지</h1> */}
+        <Slider {...settings}>
+            {productList?.category == "short" ?
+            productList.productImg.map((img) => 
+              <div className="img-box"><img className="product-img" src={require(`../img/shirts-img/short/${img}`)}>
+            </img></div>) : 
+            ""}
+            {productList?.category == "long" ?
+            productList.productImg.map((img) => 
+              <div className="img-box"><img className="product-img" src={require(`../img/shirts-img/long/${img}`)}>
+            </img></div>) : 
+            ""}
+        </Slider>
+        </div>
+        <div>
+            {productList ? <p>{productList.id}</p> : ""}
+            {productList ? <p>{productList.productName}</p> : ""}
+            {productList ? <p>{productList.price}</p> : ""}
 
-        {/* 원하는 객체가 있는지 삼항 연산자, 콘솔로 찍어봤을 때
-        거짓 경우(객체 로딩 중) -> 참 경우(객체 로딩 완료)로 넘어가면서
-        둘 다가 찍힌다.
-        
-        그래서, 로딩 되기 전의 거짓 경우와 로딩 되었을 때의 참 경우 둘 다가 필요하고,
-        객체가 있는지를 "?"를 통해 한번 더 체크해야 한다. */}
+            {/* 원하는 객체가 있는지 삼항 연산자, 콘솔로 찍어봤을 때
+            거짓 경우(객체 로딩 중) -> 참 경우(객체 로딩 완료)로 넘어가면서
+            둘 다가 찍힌다.
+            
+            그래서, 로딩 되기 전의 거짓 경우와 로딩 되었을 때의 참 경우 둘 다가 필요하고,
+            객체가 있는지를 "?"를 통해 한번 더 체크해야 한다. */}
 
-        {productList ? console.log("OK") : console.log("not yet")}
-        {productList?.category == "short" ?
-        <img src={require(`../img/shirts-img/short/${productList.productImg[0]}`)} style={{maxWidth: "30%"}}></img> : 
-        ""}
-        {productList?.category == "long" ?
-        <img src={require(`../img/shirts-img/long/${productList.productImg[0]}`)} style={{maxWidth: "30%"}}></img> : 
-        ""}
-      </div>
+            {productList ? console.log("OK") : console.log("not yet")}
+        </div>
     </div>
   );
 }
