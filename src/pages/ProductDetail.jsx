0451f9eaa47166bbc90afc3230909efcbdb9d3f4
Johnny_P <shinezl39@gmail.com>
2022-12-06 10:13:29 +0900

    canvas test

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
+import { fabric } from 'fabric';
+import 'fabric-history';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
@@ -9,9 +11,7 @@ const ProductDetail = () => {
 
   const [productList, setProductList] = useState(null);
   const [img, setImg] = useState(null);
-  const [cvsAct, setCvsAct] = useState(false);
-
-  const cvs = useRef(null);
+  const [canvas, setCanvas] = useState(null);
 
   const { id } = useParams(); // id : productList {id}
 
@@ -22,19 +22,39 @@ const ProductDetail = () => {
     setProductList(data);
   }
 
-  useEffect(() => {
-    getProduct();
-  }, [id]);
+  let deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+  let delImg = new Image();
+  delImg.src = deleteIcon;
 
-  useEffect(()=>{
-    if(productList != null) {
-      setImg(productList.productImg[0])
-    }
-  }, [productList])
+  fabric.Object.prototype.transparentCorners = false;
+  fabric.Object.prototype.cornerColor = "blue";
+  fabric.Object.prototype.cornerStyle = "circle";
 
-  useEffect(() => {
-    setCvsAct(true);
-  }, [cvsAct]);
+  fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+    x: 0.5,
+    y: -0.5,
+    offsetY: 16,
+    cursorStyle: "pointer",
+    mouseUpHandler: deleteObject,
+    render: renderIcon,
+    cornerSize: 24,
+  });
+
+  function deleteObject (eventData, transform) {
+    let target = transform.target;
+    let canvas = target.canvas;
+    canvas.remove(target);
+    canvas.requestRenderAll();
+  }
+
+  function renderIcon (ctx, left, top, styleOverride, fabricObject) {
+    let size = this.cornerSize;
+    ctx.save();
+    ctx.translate(left, top);
+    ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+    ctx.drawImage(delImg, -size / 2, -size / 2, size, size);
+    ctx.restore();
+  }
 
   const flipShirts = () => {
     for(let i = 0; i < productList.productImg.length; i++) {
@@ -46,26 +66,50 @@ const ProductDetail = () => {
       }
     }
   }
-  /*  */
 
-  const uploadCanvas = () => {
-    /* 아마도 여기서 state가 갱신되니까 캔버스가 나타나고, 렌더링이 이 시점에서 일어나니까
-    아래의 캔버스 지정과 표시가 한 번 늦게 나타나는 것 같다. */
-    let context = cvs.current.getContext("2d");
-    context.strokeRect(10, 10, 250, 130);
+  const add = () => {
+    let rect = new fabric.Rect({
+      left: 60,
+      top: 50,
+      fill: "yellow",
+      width: 100,
+      height: 100,
+      objectCaching: false,
+      stroke: "lightgreen",
+      strokeWidth: 4,
+    });
+
+    canvas.add(rect);
+    canvas.setActiveObject(rect);
   }
 
-  const dropCanvas = () => {
-    setCvsAct(false);
+  const initCanvas = () => {
+    return new fabric.Canvas('canvas', {
+      backgroundColor: "transparent"
+    })
   }
+
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, [])
+
+  useEffect(() => {
+    getProduct();
+  }, [id]);
+
+  useEffect(()=>{
+    if(productList != null) {
+      setImg(productList.productImg[0])
+    }
+  }, [productList])
 
   return (
     <div className="product-area">
 
       <div className="product-button">
         <Button variant="contained" color="success" onClick={() => {flipShirts()}}>앞/뒤</Button>
-        <Button variant="contained" color="success" onClick={() => {uploadCanvas()}}>사진 업로드</Button>
-        <Button variant="contained" color="success" onClick={() => {dropCanvas()}}>사진 삭제</Button>
+        <Button variant="contained" color="success" onClick={() => {add()}}>사진 업로드</Button>
+        <Button variant="contained" color="success" onClick={() => {}}>사진 삭제</Button>
         <Button variant="contained" color="success">텍스트</Button>
         <Button variant="contained" color="success">이미지 편집</Button>
       </div>
@@ -74,13 +118,27 @@ const ProductDetail = () => {
         {productList?.category == "short" && img != null ?
           <div className="img-box">
             <img className="product-img" src={require(`../img/shirts-img/short/${img}`)}></img>
-            {cvsAct ? <canvas ref={cvs} style={{position: "absolute", left: "0px", top: "0px", width: "100px", height: "100px"}}></canvas> : ""}
+            <canvas id="canvas" style={{
+              position: "absolute",
+              left: "0",
+              top: "0",
+              width: "100%",
+              height: "100%",
+              zIndex: "10"
+            }}></canvas>
           </div> : 
         ""}
         {productList?.category == "long" && img != null ?
           <div className="img-box">
             <img className="product-img" src={require(`../img/shirts-img/long/${img}`)}></img>
-            {cvsAct ? <canvas ref={cvs} style={{position: "absolute", left: "0px", top: "0px", width: "100px", height: "100px"}}></canvas> : ""}
+            <canvas id="canvas" style={{
+              position: "absolute",
+              left: "0",
+              top: "0",
+              width: "100%",
+              height: "100%",
+              zIndex: "10"
+            }}></canvas>
           </div> : 
         ""}
       </div>
