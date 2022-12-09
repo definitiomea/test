3f53eff195732f5a0fc5807e66573a308348ee37
Kanghj <rkdguswns0803@gmail.com>
2022-12-09 17:10:33 +0900

    20221209 login/signup 진행중

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./reducers/productReducer";
+import userReducer from "./reducers/user";
+import signupReducer from "./reducers/signup";
 
 const store = configureStore({
-    reducer: {
-        product: productReducer
-    }
+  reducer: {
+    product: productReducer,
+    user: userReducer,
+    signup: signupReducer,
+  },
 });
 
 export default store;
