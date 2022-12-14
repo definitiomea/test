e6521d0b58d1d486a5d3c16ca594c3df8853c9c1
sk Lee <znfnfn2140@gmail.com>
2022-12-14 10:06:05 +0900

    로딩 시 보일 컴프

--- src/components/Loding.jsx
+++ src/components/Loding.jsx
@@ -14,7 +14,7 @@ const Loading = () => {
 export default Loading;
 
 const LoadingWrap = styled.div`
-  min-height: calc(100vh - 160px); // 푸터 높이 + 네브 높이
+  min-height: calc(100vh - 160px); // 160px : 푸터 높이 + 네브 높이
   display: flex;
   > div {
     margin: auto;
