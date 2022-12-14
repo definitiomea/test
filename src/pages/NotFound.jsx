80dd7a5ccc1f77d0a3b06ef261433110dc5c852c
sk Lee <znfnfn2140@gmail.com>
2022-12-14 11:48:00 +0900

    404page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,60 @@
+import styled from "styled-components";
+import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="wrap">
-      <h1>페이지가 존재하지 않습니다</h1>
-      <button onClick={() => {navigate("/")}}>홈으로 가기</button>
-    </div>
+    <ErrorWrap>
+      <div>
+        <h1>OOPS!</h1>
+        <p>We couldn't find the page your looking for.</p>
+        <Button
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          VISIT HOMEPAGE
+        </Button>
+        <Button
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          GO BACK
+        </Button>
+      </div>
+    </ErrorWrap>
   );
 };
 
 export default NotFound;
+
+const ErrorWrap = styled.div`
+  position: relative;
+  height: calc(100vh - 160px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${"h1"} {
+    color: #adb5bd;
+    font-size: 4rem;
+    font-weight: bold;
+    margin-bottom: 1.5rem;
+  }
+  ${"p"} {
+    margin-bottom: 5rem;
+  }
+  .MuiButtonBase-root {
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+    color: #f8f9fa;
+    background-color: #212529;
+    font-weight: bold;
+    &:hover {
+      background-color: black;
+    }
+  }
+`;
