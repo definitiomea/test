9a631aa6381dd9197eef1a9548987a1877c29a69
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-12-22 17:41:40 +0900

    상위 페이지에서 props -> 모듈에도 props.. 분리할 수는 있는데 의미있을지

--- src/components/ProductButton.jsx
+++ src/components/ProductButton.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Button from '@mui/material/Button'
+
+/*  */
 
 const ProductButton = () => {
   return (
-    <div>
-      
+    <div className='product-button'>
+      <Button>앞/뒤 뒤집기</Button>
+      <input type='file' accept='image/*' onChange={(event) => {}}></input>
+      <Button>텍스트 추가하기</Button>
+      <input type='color' onChange={(event) => {}}></input>
+      <Button>편집 되돌리기</Button>
+      <Button>편집 되돌리기 취소</Button>
+      <Button>편집 내역 초기화</Button>
+      <Button>이미지 내보내기(Dispatch)</Button>
+      <Button>편집한 면의 이미지 저장</Button>
+      <Button>이미지 저장 내역 지우기</Button>
     </div>
   )
 }
