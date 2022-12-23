6f918b56c8644e39ae429b7e08347e6c5dd4b467
sk LEE <znfnfn2140@gmail.com>
2022-12-23 22:47:22 +0900

    Merge branch 'seung-yeon' into 'sun-kyeong'

--- src/redux/reducers/reviewInputReducer.js
+++ src/redux/reducers/reviewInputReducer.js
@@ -1,20 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  reviewStar: null,
-  reviewContent: null,
-  reviewImg: null,
+  reviewID: 0,
+  reviewInput: {
+    userID: "",
+    img: "",
+    category: "",
+    productName: "",
+    size: "",
+    comment: "",
+    date: "",
+  },
 };
 
-export const reviewSlice = createSlice({
+const reviewSlice = createSlice({
   name: "reviewInput",
   initialState,
   reducers: {
     addReview: (state, action) => {
       const newReview = {
+        // 리뷰 첨부 이미지 불러오기
+        imgArray: action.payload.img,
+        // 고객 로그인 아이디 가져오기
         userID: action.payload.userID,
-        img: action.payload.img,
+        // 리뷰 별점 가져오기
+        // star:,
+        // 제품 종류 불러오기
+        category: action.payload.category,
+        // 제품 스타일 불러오기
+        productName: action.payload.productName,
+        // 사이즈 불러오기
+        size: action.payload.size,
+        // // 리뷰 작성내용 불러오기
+        // comment:,
+        // // 작성 날짜 불러오기
+        // date:,
       };
+      const newReviewlist = state.reviewInput.concat(addReview);
+      state.reviewInput = newReviewlist;
+    },
+    deleteReview: (state, action) => {
+      // reviewInput배열에서 reiviewID가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
+      const newReviewlist = state.reviewInput.filter(
+        (review) => review.reviewID != action.payload
+      );
     },
   },
 
@@ -31,5 +60,5 @@ export const reviewSlice = createSlice({
   // },
 });
 
-export const { ADD_IMG, ADD_STAR, ADD_CONTENT } = reviewSlice.actions;
+export const { addReview, deleteReview } = reviewSlice.actions;
 export default reviewSlice.reducer;
