da0a0a4bac7b1070bea2bf7661efb67f88f375a9
sk LEE <znfnfn2140@gmail.com>
2022-12-24 10:59:05 +0900

    마이페이지 merge 1224

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import DaumPostcodeEmbed from "./DeliveryList";
-import Slider from "react-slick";
 import { useEffect, useState } from "react";
 import { Modal } from "@mui/material";
 import { Box } from "@mui/system";
 import Delivery from "./Delivery";
+import { useDispatch, useSelector } from "react-redux";
+import { ADDIT_USER } from "../redux/reducers/signup";
+import { loginUser } from "../redux/reducers/user";
 
 const Mypage = () => {
   // 택배사 목록 state
@@ -14,13 +16,21 @@ const Mypage = () => {
   const [trackId, setTrackId] = useState("");
   const [carrierId, setCarrierId] = useState("");
   const [result, setResult] = useState(true);
+  const [trans, setTrans] = useState(null);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
     setResult(true);
   };
-  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const additUser = {
+      ...user,
+      [e.target.name]: e.target.value,
+    };
+    setTrans(additUser);
+  };
 
   const changeCarrierId = (e) => {
     setCarrierId(e.target.value);
@@ -56,13 +66,10 @@ const Mypage = () => {
     getCarriers();
   }, []);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  const user = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
   const style = {
     position: "absolute",
     top: "50%",
@@ -75,6 +82,9 @@ const Mypage = () => {
     p: 4,
   };
 
+  // 주문완료 섹션 출력 함수
+  const orderDone = useSelector((state) => state.orderlist.orderlist);
+
   return (
     <Body>
       {/* 회원정보 수정 form */}
@@ -82,24 +92,49 @@ const Mypage = () => {
 
       <UserInfo>
         <Labels className="labels">
-          <label>이름</label>
-          <label>휴대폰 번호</label>
-          <label>아이디</label>
-          <label>비밀번호 변경</label>
-          <label>비밀번호 확인</label>
+          <label>id</label>
+          <label>email</label>
+          <label>password</label>
+          <label>password</label>
+          {/* <label>비밀번호 확인</label> */}
         </Labels>
 
-        <Inputs>
-          <input type="text" defaultValue="홍길동" />
-          <input type="text" defaultValue="010-****-1234" />
-          <input type="text" defaultValue="roadBronze" />
-          <input type="password" placeholder="비밀번호를 입력하세요" />
-          <input type="password" placeholder="비밀번호를 입력하세요" />
-        </Inputs>
+        <Inputs
+          onSubmit={(e) => {
+            dispatch(ADDIT_USER(trans));
+            dispatch(loginUser(trans));
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            name="id"
+            defaultValue={user.id}
+            onChange={onChange}
+          />
+          <input
+            type="text"
+            name="email"
+            defaultValue={user.email}
+            onChange={onChange}
+          />
 
+          <input
+            type="password"
+            name="password"
+            defaultValue={user.password}
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder={user.password}
+            onChange={onChange}
+          />
+          <button>회원정보 수정</button>
+        </Inputs>
         <DaumPostcodeEmbed />
       </UserInfo>
-      <button>회원정보 수정</button>
 
       {/* 주문/배송조회 form  */}
       <MypageOrder>
@@ -204,49 +239,57 @@ const Mypage = () => {
         </MypageBody>
       </MypageOrder>
 
-      {/* 이벤트 배너 form  */}
-      <MypageEvent>
-        <H4>이벤트</H4>
-      </MypageEvent>
-      <div
-        style={{ width: "100%", height: "200px", backgroundColor: "skyblue" }}
-      >
-        <Slider {...settings}>
-          <div>
-            <img
-              src="https://foremanbrosinc.com/wp-content/uploads/2017/05/1c0d0f0cb8b7f2fb2685da9798efe42b_big-image-png-image-placeholder-clipart_2400-2400-300x300.png"
-              alt=""
-              onClick={() => navigate("/event")}
-              style={{
-                width: "100%",
-                height: "50px",
-              }}
-            />
-          </div>
-          <div>
-            <img
-              src="https://foremanbrosinc.com/wp-content/uploads/2017/05/1c0d0f0cb8b7f2fb2685da9798efe42b_big-image-png-image-placeholder-clipart_2400-2400-300x300.png"
-              alt=""
-              onClick={() => navigate("/event")}
-              style={{
-                width: "100%",
-                height: "50px",
-              }}
-            />
-          </div>
-          <div>
-            <img
-              src="https://foremanbrosinc.com/wp-content/uploads/2017/05/1c0d0f0cb8b7f2fb2685da9798efe42b_big-image-png-image-placeholder-clipart_2400-2400-300x300.png"
-              alt=""
-              onClick={() => navigate("/event")}
-              style={{
-                width: "100%",
-                height: "50px",
-              }}
-            />
-          </div>
-        </Slider>
-      </div>
+      {/* 주문완료 섹션 */}
+      {orderDone.map((re) =>
+        re.orderID == 3 ? (
+          <MypageBody>
+            <MypagePd>
+              <div>
+                <img
+                  className="img"
+                  src={require(`.././img/shirts-img/short/short-relax-beige-front.jpg`)}
+                  alt="#"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                />
+              </div>
+              <MypageInfo>
+                {/* 상품 정보 */}
+                <div>
+                  <span>{re.category} </span>
+                  <span>{re.productName} </span>
+                  <span> ({re.color}) </span>
+                </div>
+
+                {/* 사이즈 정보 */}
+                <div>
+                  <span>size : </span>
+                  <span>{re.size}</span>
+                </div>
+              </MypageInfo>
+            </MypagePd>
+
+            <div></div>
+
+            <MypageColum>
+              <div>{re.price}</div>
+              <div>{re.quantity}개</div> {/* 연한 회색 처리 */}
+            </MypageColum>
+
+            <MypageColum>
+              <div>
+                <Link to="/mypage/review" state={{ orderDone: orderDone[2] }}>
+                  후기작성
+                </Link>
+              </div>
+            </MypageColum>
+          </MypageBody>
+        ) : (
+          ""
+        )
+      )}
     </Body>
   );
 };
