30c649a6731c21994ea92fe5e44ef31984cea77b
sk LEE <znfnfn2140@gmail.com>
2023-01-03 00:44:46 +0900

    구매내역 더미데이터, 리듀서 수정

--- src/redux/reducers/order.js
+++ src/redux/reducers/order.js
@@ -12,44 +12,44 @@ const getDate = () => {
 
 // 초기값
 const initialState = {
-  orderID: 2,
+  orderID: 3,
   orderlist: [
     {
-      user: "abc123", // 현재로그인한 아이디가 들어가도록 만들고, 테스트용이라 명시하기
-      orderID: 1,
-      orderDate: "2022.12.24",
-      productID: 2,
-      category: "short",
-      productName: "스탠다드 핏",
-      color: "blue",
-      size: "L",
-      quantity: 8,
-      imgArray: [{ print: "front", imageUrl: "" }],
-      thumbnail: "short-standard-blue-front.jpg",
-      totalPay: 76000,
-      delivery: "배송완료",
-    },
-    { 
-      user: "abc123",
-      orderID: 2,
-      orderDate: "2022.12.28",
-      productID: 5,
-      category: "long",
-      productName: "스탠다드 핏",
-      color: "cream",
-      size: "S",
-      quantity: 4,
-      imgArray: [
-        { print: "front", imageUrl: "" },
-        { print: "back", imageUrl: "" },
+      userId: "abc123",
+      itemlist: [
+        {
+          orderID: 1,
+          orderDate: "2022.12.24",
+          productID: 2,
+          category: "short",
+          productName: "스탠다드 핏",
+          color: "blue",
+          size: "L",
+          quantity: 8,
+          imgArray: [{ print: "front", imageUrl: "" }],
+          thumbnail: "short-standard-blue-front.jpg",
+          totalPay: 76000,
+          delivery: "배송완료",
+        },
+        {
+          orderID: 2,
+          orderDate: "2022.12.28",
+          productID: 5,
+          category: "long",
+          productName: "스탠다드 핏",
+          color: "cream",
+          size: "S",
+          quantity: 4,
+          imgArray: [
+            { print: "front", imageUrl: "" },
+            { print: "back", imageUrl: "" },
+          ],
+          thumbnail: "long-standard-cream-front.jpg",
+          totalPay: 38000,
+          delivery: "상품준비",
+        },
       ],
-      thumbnail: "long-standard-cream-front.jpg",
-      totalPay: 38000,
-      delivery: "상품준비",
     },
-    { 
-      // 다른아이디 더미 1개 - 구매내역 필터 적용
-    }
   ],
 };
 
@@ -61,22 +61,52 @@ const orderSlice = createSlice({
     inputOrder: (state, action) => {
       const cartlist = action.payload.cartlist;
       for (let i = 0; i < cartlist.length; i++) {
-        cartlist[i].user = action.payload.user;
-        cartlist[i].orderID = ++state.orderID;
+        cartlist[i].orderID = state.orderID++;
         cartlist[i].orderDate = getDate();
         cartlist[i].delivery = "상품준비";
         delete cartlist[i].cartID;
       }
-      const newOrderlist = state.orderlist.concat(cartlist);
-      state.orderlist = newOrderlist;
-    },
-    // 회원가입을 할 때, 후기작성 기능을 확인하기 위한 더미데이터 추가
-    AddDummyData: (state, action) => {
 
-    }
+      const index = state.orderlist.findIndex(
+        (el) => el.userId === action.payload.user
+      );
+      if (index === -1) {
+        state.orderlist = state.orderlist.concat({
+          userId: action.payload.user,
+          itemlist: cartlist,
+        });
+      } else {
+        const newItemlist = state.orderlist[index].itemlist.concat(cartlist);
+        state.orderlist[index] = {
+          ...state.orderlist[index],
+          itemlist: newItemlist,
+        };
+      }
+    },
+    // 회원가입을 할 때, 후기작성 기능을 확인하기 위해 배송이 완료된 더미데이터 추가
+    AddDummyData: (state, action) => {
+      const data = {
+        orderID: state.orderID++,
+        orderDate: getDate(),
+        productID: 2,
+        category: "short",
+        productName: "스탠다드 핏",
+        color: "blue",
+        size: "L",
+        quantity: 8,
+        imgArray: [{ print: "front", imageUrl: "" }],
+        thumbnail: "short-standard-blue-front.jpg",
+        totalPay: 76000,
+        delivery: "배송완료",
+      };
+      state.orderlist = state.orderlist.concat({
+        userId: action.payload.user,
+        itemlist: [data],
+      });
+    },
   },
 });
 
-export const { inputOrder } = orderSlice.actions;
+export const { inputOrder, AddDummyData } = orderSlice.actions;
 
 export default orderSlice.reducer;
